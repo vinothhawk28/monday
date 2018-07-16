f8d04a1f8d8c3564fbed892d4164535ee32ce5fd
Vinoth <38490375+vinothhawk28@users.noreply.github.com>
2018-07-16 14:35:55 +0530

    Create Roundoff.java

---
+++ Roundoff.java
@@ -0,0 +1,19 @@
+package guvi;
+import java.util.*;
+public class roundoff {
+public static void main(String[] args) {
+	double a;
+	int d=0;
+	Scanner in=new Scanner(System.in);
+	a=in.nextDouble();
+	double s=a-(int)a;
+	if(s>=0.1){
+		d=(int)a+1;
+	System.out.println(d);	}
+	else{
+		d=(int)a;
+		System.out.println(d);
+	}
+	}
+}
+
